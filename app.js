495d47b68a7ab50a0a780fcb06d6d49d91945a94
KrysZyb <87859000+KrysZyb@users.noreply.github.com>
2021-08-07 08:59:27 -0400

    testing pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Beanstock Test LEts GO!'));
+app.get('/', (req, res) => res.send('Hi Stephen - how is your PS4 game going?'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
